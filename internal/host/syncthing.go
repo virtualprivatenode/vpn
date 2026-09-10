15194a07dd18dc4dc3c7b3718f2c9337b6eca803
ripsline <ripsline@protonmail.com>
2026-09-10 17:00:01 -0400

    refactor: complete syncthing identity command arguments

--- internal/host/syncthing.go
+++ internal/host/syncthing.go
@@ -12,7 +12,10 @@ import (
 // read-only command. Configuration device order does not identify the local node.
 // runuser supplies the service HOME required by Syncthing package initialization.
 func SyncthingDeviceID() string {
-	output, err := system.RunContext(10*time.Second, "runuser", "-u", "syncthing", "--", paths.SyncthingBinary, "device-id", "--config="+paths.SyncthingDir)
+	output, err := system.RunContext(10*time.Second,
+		"runuser", "-u", "syncthing", "--", paths.SyncthingBinary,
+		"device-id", "--config="+paths.SyncthingDir,
+		"--data="+paths.SyncthingDataDir)
 	if err != nil {
 		return ""
 	}
